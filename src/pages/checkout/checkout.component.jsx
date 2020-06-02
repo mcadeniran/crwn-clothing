9efb2abbd6cde44fa788510c16c777e88bf7bcc7
Kolawole Adeniran <kolamaster@gmail.com>
2020-06-03 02:23:48 +0300

    implemented strip payment and also changed title and favico

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>Total: ${total}</span>
     </div>
+    <div className='test-warning'>
+      Please use the following test credit card for payments*
+      <br />
+      4242 42424 4242 4242 - Exp: any future date - CVV: Any 3 digits
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
